226a1abc04c524ec6c77b1b42e432fbb0f0d1f5b
JanChristiansen <christiansen@myhammer.de>
2010-03-23 12:51:14 +0100

    fixed syntax error

--- config.inc.sample.php
+++ config.inc.sample.php
@@ -2,7 +2,7 @@
 
 // rename to "conifg.inc.php" and fill in the necessary values:
 
-define('MYSQL_SERVER', 127.0.0.1');
+define('MYSQL_SERVER', '127.0.0.1');
 define('MYSQL_USER', 'root');
 define('MYSQL_PASSWD', '');
 define('MYSQL_DATABASE', 'test');
